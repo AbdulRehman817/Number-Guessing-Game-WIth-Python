ca0b4eb64c9e4d3a208ad0a06e6d3a8df14bd19e
AbdulRehman817 <abdulrehmanbey1718@gmail.com>
2026-05-05 16:12:19 +0500

    Added difficuly levels

--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
 import random
 numbers=[1,2,3,4,5,6,7,8,9,10];
+difficulty=input("\nChoose difficuly (Easy,Hard,Medium): ");
+if difficulty == "Easy":
+    numbers=list(range(1,11))
+elif difficulty == "Medium":
+    numbers=list(range(10,21))
+else:
+    numbers=list(range(22,36))
 while True:
     
     user_input=int(input("\nGuess number: "));
     choices=random.choice(numbers);
     diff=abs(user_input-choices)
+    
     if diff==0:
         print("Correct",user_input," ",choices);
         break
