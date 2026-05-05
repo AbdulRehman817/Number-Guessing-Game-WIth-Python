5ae9f81b7a9c5afa18154573a12b0c9aba866279
AbdulRehman817 <abdulrehmanbey1718@gmail.com>
2026-05-05 16:31:30 +0500

    Added Value exception Handler and want to play again feature

--- hello.py
+++ hello.py
@@ -1,28 +1,38 @@
 import random
 numbers=[1,2,3,4,5,6,7,8,9,10];
-difficulty=input("\nChoose difficuly (Easy,Hard,Medium): ");
-if difficulty == "Easy":
-    numbers=list(range(1,11))
-elif difficulty == "Medium":
-    numbers=list(range(10,21))
-else:
-    numbers=list(range(22,36))
 while True:
-    
-    user_input=int(input("\nGuess number: "));
-    choices=random.choice(numbers);
-    diff=abs(user_input-choices)
-    
-    if diff==0:
-        print("Correct",user_input," ",choices);
-        break
-    elif diff <=2:
-        print("🔥 Very close!",user_input," ",choices);
-    elif diff <=5:
-            print("😐 Getting warmer...",user_input," ",choices);
+    difficulty=input("\nChoose difficuly (Easy,Hard,Medium): ");
+    if difficulty == "easy":
+        numbers = list(range(1, 11))
+    elif difficulty == "medium":
+        numbers = list(range(10, 21))
+    elif difficulty == "hard":
+        numbers = list(range(22, 36))   
     else:
-        print("🧊 Ice cold!",user_input," ",choices);
+        print("Invalid choice! Defaulting to Easy.")  # 👈 Handle wrong input
+        numbers = list(range(1, 11))
+    while True:
+        try:
+            user_input=int(input("\nGuess number: "));
+            choices=random.choice(numbers);
+            diff=abs(user_input-choices)
+    
+            if diff==0:
+                print("Correct",user_input," ",choices);
+                break
+            elif diff <=2:
+                print("🔥 Very close!",user_input," ",choices);
+            elif diff <=5:
+                print("😐 Getting warmer...",user_input," ",choices);
+            else:
+                print("🧊 Ice cold!",user_input," ",choices);
 
+        except ValueError:
+            print("Please enter valid number");
+            continue
 
-
-
+    play_again=input("\nWant to play y/n ")
+    if play_again !="y":
+        print("Thanks for playing");
+        break
+    
